bd1c70f71aa8359c75f3d9c9ee09d6a3fe9a9853
nikesh <knikesh5601@gmail.com>
2024-03-05 14:00:13 +0530

    Adding

---
+++ RoleAuthenticationApi/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoleAuthenticationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+    }
+}
